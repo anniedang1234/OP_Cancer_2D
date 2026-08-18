7dcc1cce401327d041908d25be1e2b80d3aa8037
anniedang1234 <147004605+anniedang1234@users.noreply.github.com>
2026-08-18 16:07:39 -0400

    Update OP_Cancer_2D.py

--- Simulation/OP_Cancer_2D.py
+++ Simulation/OP_Cancer_2D.py
@@ -1,22 +1,300 @@
 
+def configure_simulation():
+    
+    import csv
+    
+    # Open file
+    
+    parameters_file = r"/home/annied/OP_Cancer_2D/parameters.csv"
+
+    # Read parameters
+
+    with open(parameters_file, newline='') as f:
+        
+        reader = csv.reader(f)
+        
+        for i, line in enumerate(reader):
+            
+            if i == 1:
+                ifn_diffusion = float(line[1])
+            elif i == 2:
+                ifn_decay = float(line[1])
+            elif i == 3:
+                ifn_initial_conc = float(line[1])
+            elif i == 4:
+                tgf_diffusion = float(line[1])
+            elif i == 5:
+                tgf_decay = float(line[1])
+            elif i == 6:
+                tgf_initial_conc = float(line[1])
+            elif i == 7:
+                collagen_initial_conc = float(line[1])
+            
+    f.close()
+    
+    
+    from cc3d.core.XMLUtils import ElementCC3D
+    
+    CompuCell3DElmnt=ElementCC3D("CompuCell3D",{"Revision":"5","Version":"4.7.0"})
+    
+    MetadataElmnt=CompuCell3DElmnt.ElementCC3D("Metadata")
+    
+    # Basic properties of simulation
+    MetadataElmnt.ElementCC3D("NumberOfProcessors",{},"4")
+    MetadataElmnt.ElementCC3D("DebugOutputFrequency",{},"10")
+    MetadataElmnt.ElementCC3D("MCSConversionFactor",{"DisplayName":"No conversion","Units":"-"},"1.0")
+    MetadataElmnt.ElementCC3D("VoxelConversionFactor",{"DisplayName":"No conversion","Units":"-"},"1.0")
+    
+    # Basic properties of CPM (GGH) algorithm
+    PottsElmnt=CompuCell3DElmnt.ElementCC3D("Potts")
+    PottsElmnt.ElementCC3D("Dimensions",{"x":"800","y":"1420","z":"1"})
+    PottsElmnt.ElementCC3D("Steps",{},"2520")
+    PottsElmnt.ElementCC3D("Temperature",{},"3")
+    PottsElmnt.ElementCC3D("NeighborOrder",{},"3")
+    
+    # Cell types
+    PluginElmnt=CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"CellType"})
+    PluginElmnt.ElementCC3D("CellType",{"TypeId":"0","TypeName":"Medium"})
+    PluginElmnt.ElementCC3D("CellType",{"TypeId":"1","TypeName":"Tumour"})
+    PluginElmnt.ElementCC3D("CellType",{"TypeId":"2","TypeName":"CAF"})
+    PluginElmnt.ElementCC3D("CellType",{"TypeId":"3","TypeName":"myCAF"})
+    PluginElmnt.ElementCC3D("CellType",{"TypeId":"4","TypeName":"CD8T"})
+    
+    
+    CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"Volume"})
+    
+    CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"CenterOfMass"})
+    
+    # Module tracking neighbouring cells of each cell
+    CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"NeighborTracker"})
+    
+    # Specification of adhesion energies
+    CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"ExternalPotential"})
+    PluginElmnt_1=CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"Contact"})
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Medium","Type2":"Medium"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Medium","Type2":"Tumour"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Medium","Type2":"CAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Medium","Type2":"myCAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Medium","Type2":"CD8T"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Tumour","Type2":"Tumour"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Tumour","Type2":"CAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Tumour","Type2":"myCAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"Tumour","Type2":"CD8T"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"CAF","Type2":"CAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"CAF","Type2":"myCAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"CAF","Type2":"CD8T"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"CD8T","Type2":"CD8T"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"CD8T","Type2":"myCAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("Energy",{"Type1":"myCAF","Type2":"myCAF"},"10.0")
+    PluginElmnt_1.ElementCC3D("NeighborOrder",{},"1")
+    
+    # Specification of focal point junctions
+    PluginElmnt_2=CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"FocalPointPlasticity"})
+    
+    
+    ParametersElmnt=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"Tumour","Type2":"Tumour"})
+    ParametersElmnt.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_1=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"Tumour","Type2":"CAF"})
+    ParametersElmnt_1.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_1.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_1.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_1.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_1.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_2=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"Tumour","Type2":"myCAF"})
+    ParametersElmnt_2.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_2.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_2.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_2.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_2.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_3=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"Tumour","Type2":"CD8T"})
+    ParametersElmnt_3.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_3.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_3.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_3.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_3.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_4=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"CAF","Type2":"CAF"})
+    ParametersElmnt_4.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_4.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_4.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_4.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_4.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_5=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"CAF","Type2":"myCAF"})
+    ParametersElmnt_5.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_5.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_5.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_5.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_5.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_6=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"CAF","Type2":"CD8T"})
+    ParametersElmnt_6.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_6.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_6.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_6.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_6.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_7=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"myCAF","Type2":"myCAF"})
+    ParametersElmnt_7.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_7.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_7.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_7.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_7.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_8=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"myCAF","Type2":"CD8T"})
+    ParametersElmnt_8.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_8.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_8.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_8.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_8.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    ParametersElmnt_9=PluginElmnt_2.ElementCC3D("Parameters",{"Type1":"CD8T","Type2":"CD8T"})
+    ParametersElmnt_9.ElementCC3D("Lambda",{},"10")
+    ParametersElmnt_9.ElementCC3D("ActivationEnergy",{},"-50")
+    ParametersElmnt_9.ElementCC3D("TargetDistance",{},"7")
+    ParametersElmnt_9.ElementCC3D("MaxDistance",{},"20")
+    ParametersElmnt_9.ElementCC3D("MaxNumberOfJunctions",{"NeighborOrder":"1"},"1")
+    
+    
+    PluginElmnt_2.ElementCC3D("NeighborOrder",{},"1")
+    
+    # Connectivity constraint applied to each cell. Energy penalty specifies how strong the constraint is. Penalty is specified for each type.
+    PluginElmnt_3=CompuCell3DElmnt.ElementCC3D("Plugin",{"Name":"ConnectivityGlobal"})
+    PluginElmnt_3.ElementCC3D("Penalty",{"Type":"Tumour"},"1000000")
+    PluginElmnt_3.ElementCC3D("Penalty",{"Type":"CAF"},"1000000")
+    PluginElmnt_3.ElementCC3D("Penalty",{"Type":"myCAF"},"1000000")
+    PluginElmnt_3.ElementCC3D("Penalty",{"Type":"CD8T"},"1000000")
+    
+    # Specification of PDE solvers
+    SteppableElmnt=CompuCell3DElmnt.ElementCC3D("Steppable",{"Type":"DiffusionSolverFE_OpenCL"})
+    
+    
+    DiffusionFieldElmnt=SteppableElmnt.ElementCC3D("DiffusionField",{"Name":"IFN_gamma"})
+    DiffusionDataElmnt=DiffusionFieldElmnt.ElementCC3D("DiffusionData")
+    DiffusionDataElmnt.ElementCC3D("FieldName",{},"IFN_gamma")
+    DiffusionDataElmnt.ElementCC3D("GlobalDiffusionConstant",{},ifn_diffusion)
+    DiffusionDataElmnt.ElementCC3D("GlobalDecayConstant",{},ifn_decay)
+    DiffusionDataElmnt.ElementCC3D("InitialConcentrationExpression",{},ifn_initial_conc)
+    DiffusionFieldElmnt.ElementCC3D("SecretionData")
+    
+    
+    BoundaryConditionsElmnt=DiffusionFieldElmnt.ElementCC3D("BoundaryConditions")
+    
+    
+    PlaneElmnt=BoundaryConditionsElmnt.ElementCC3D("Plane",{"Axis":"X"})
+    PlaneElmnt.ElementCC3D("ConstantDerivative",{"PlanePosition":"Min","Value":"0.0"})
+    PlaneElmnt.ElementCC3D("ConstantDerivative",{"PlanePosition":"Max","Value":"0.0"})
+    PlaneElmnt_1=BoundaryConditionsElmnt.ElementCC3D("Plane",{"Axis":"Y"})
+    PlaneElmnt_1.ElementCC3D("ConstantDerivative",{"PlanePosition":"Min","Value":"0.0"})
+    PlaneElmnt_1.ElementCC3D("ConstantDerivative",{"PlanePosition":"Max","Value":"0.0"})
+    
+    
+    DiffusionFieldElmnt_1=SteppableElmnt.ElementCC3D("DiffusionField",{"Name":"TGF_beta"})
+    DiffusionDataElmnt_1=DiffusionFieldElmnt_1.ElementCC3D("DiffusionData")
+    DiffusionDataElmnt_1.ElementCC3D("FieldName",{},"TGF_beta")
+    DiffusionDataElmnt_1.ElementCC3D("GlobalDiffusionConstant",{},tgf_diffusion)
+    DiffusionDataElmnt_1.ElementCC3D("GlobalDecayConstant",{},tgf_decay)
+    DiffusionDataElmnt_1.ElementCC3D("InitialConcentrationExpression",{},tgf_initial_conc)
+    DiffusionFieldElmnt_1.ElementCC3D("SecretionData")
+    
+    
+    BoundaryConditionsElmnt_1=DiffusionFieldElmnt_1.ElementCC3D("BoundaryConditions")
+    
+    
+    PlaneElmnt_2=BoundaryConditionsElmnt_1.ElementCC3D("Plane",{"Axis":"X"})
+    PlaneElmnt_2.ElementCC3D("ConstantDerivative",{"PlanePosition":"Min","Value":"0.0"})
+    PlaneElmnt_2.ElementCC3D("ConstantDerivative",{"PlanePosition":"Max","Value":"0.0"})
+    PlaneElmnt_3=BoundaryConditionsElmnt_1.ElementCC3D("Plane",{"Axis":"Y"})
+    PlaneElmnt_3.ElementCC3D("ConstantDerivative",{"PlanePosition":"Min","Value":"0.0"})
+    PlaneElmnt_3.ElementCC3D("ConstantDerivative",{"PlanePosition":"Max","Value":"0.0"})
+    
+    
+    DiffusionFieldElmnt_2=SteppableElmnt.ElementCC3D("DiffusionField",{"Name":"Collagen"})
+    
+    
+    DiffusionDataElmnt_2=DiffusionFieldElmnt_2.ElementCC3D("DiffusionData")
+    DiffusionDataElmnt_2.ElementCC3D("FieldName",{},"Collagen")
+    DiffusionDataElmnt_2.ElementCC3D("GlobalDiffusionConstant",{},"0")
+    DiffusionDataElmnt_2.ElementCC3D("GlobalDecayConstant",{},"0")
+    DiffusionDataElmnt_2.ElementCC3D("InitialConcentrationExpression",{},collagen_initial_conc)
+    
+    # Secretion
+    DiffusionFieldElmnt_2.ElementCC3D("SecretionData")
+    
+    
+    BoundaryConditionsElmnt_2=DiffusionFieldElmnt_2.ElementCC3D("BoundaryConditions")
+    
+    
+    PlaneElmnt_4=BoundaryConditionsElmnt_2.ElementCC3D("Plane",{"Axis":"X"})
+    PlaneElmnt_4.ElementCC3D("ConstantDerivative",{"PlanePosition":"Min","Value":"0.0"})
+    PlaneElmnt_4.ElementCC3D("ConstantDerivative",{"PlanePosition":"Max","Value":"0.0"})
+    
+    
+    PlaneElmnt_5=BoundaryConditionsElmnt_2.ElementCC3D("Plane",{"Axis":"Y"})
+    PlaneElmnt_5.ElementCC3D("ConstantDerivative",{"PlanePosition":"Min","Value":"0.0"})
+    PlaneElmnt_5.ElementCC3D("ConstantDerivative",{"PlanePosition":"Max","Value":"0.0"})
+
+
+    CompuCellSetup.setSimulationXMLDescription(CompuCell3DElmnt)    
+
+
+
 from cc3d import CompuCellSetup
 
+configure_simulation()            
 
-###################################################
-## CLASSES FOR SIMULATING BIOPHYSICAL MECHANISMS ##
-###################################################
+
+##############################
+## CLASSES FOR INITIALIZING ##
+##############################
 
 from OP_Cancer_2DSteppables import HelperFunctionsSteppable
 CompuCellSetup.register_steppable(steppable=HelperFunctionsSteppable(frequency=1))          
 
 from OP_Cancer_2DSteppables import InitializeCellPositionSteppable
-CompuCellSetup.register_steppable(steppable=InitializeCellPositionSteppable(frequency=1))          
+CompuCellSetup.register_steppable(steppable=InitializeCellPositionSteppable(frequency=1))
+
+
+#################################
+## CLASSES FOR OUTPUTTING DATA ##
+#################################
+
+from OP_Cancer_2DSteppables import OutputCSVSteppable
+CompuCellSetup.register_steppable(steppable=OutputCSVSteppable(frequency=1))
+
+
+##################################
+## CLASSES FOR BASIC MECHANISMS ##
+##################################   
+
 
 from OP_Cancer_2DSteppables import GrowthSteppable
 CompuCellSetup.register_steppable(steppable=GrowthSteppable(frequency=1))
 
 from OP_Cancer_2DSteppables import MitosisSteppable
 CompuCellSetup.register_steppable(steppable=MitosisSteppable(frequency=1))
+
+
+##########################################
+## CLASSES FOR UPDATING CELL PROPERTIES ##
+##########################################
+
 
 from OP_Cancer_2DSteppables import UpdateTumourCellsSteppable
 CompuCellSetup.register_steppable(steppable=UpdateTumourCellsSteppable(frequency=1))
@@ -27,30 +305,34 @@ CompuCellSetup.register_steppable(steppable=UpdateCAFsSteppable(frequency=1))
 from OP_Cancer_2DSteppables import UpdateCD8TCellsSteppable
 CompuCellSetup.register_steppable(steppable=UpdateCD8TCellsSteppable(frequency=1))
 
+
+###############################
+## CLASSES FOR CELL MOVEMENT ##
+###############################
+
+
 from OP_Cancer_2DSteppables import CD8TCellsMoveSteppable
 CompuCellSetup.register_steppable(steppable=CD8TCellsMoveSteppable(frequency=1))
+
+from OP_Cancer_2DSteppables import TumourCellsMoveSteppable
+CompuCellSetup.register_steppable(steppable=TumourCellsMoveSteppable(frequency=1))
+
+from OP_Cancer_2DSteppables import CAFsMoveSteppable
+CompuCellSetup.register_steppable(steppable=CAFsMoveSteppable(frequency=1))
 
 
 ########################################     
 ## CLASSES FOR CALIBRATING PARAMETERS ##
 ########################################
 
-from OP_Cancer_2DSteppables import CellSpeedTrackerSteppable
-CompuCellSetup.register_steppable(steppable=CellSpeedTrackerSteppable(frequency=1))
+from OP_Cancer_2DSteppables import CellMigrationTrackerSteppable
+CompuCellSetup.register_steppable(steppable=CellMigrationTrackerSteppable(frequency=1))
 
 from OP_Cancer_2DSteppables import CD8TKillAttemptsTrackerSteppable
 CompuCellSetup.register_steppable(steppable=CD8TKillAttemptsTrackerSteppable(frequency=1))
 
 
-#################################
-## CLASSES FOR OUTPUTTING DATA ##
-#################################
 
-from OP_Cancer_2DSteppables import OutputCSVSteppable
-CompuCellSetup.register_steppable(steppable=OutputCSVSteppable(frequency=1))
-
-from OP_Cancer_2DSteppables import PlotsSteppable
-CompuCellSetup.register_steppable(steppable=PlotsSteppable(frequency=1))
 
 
 
